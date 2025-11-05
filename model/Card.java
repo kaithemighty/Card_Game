dff5d9f631d3640977acc7f3190f9ea3db8f68d1
Sekai Cuperus <ck.cuperus@icloud.com>
2025-11-04 20:09:40 -0800

    Update Repo with Code

--- model/Card.java
+++ model/Card.java
@@ -10,5 +10,25 @@ package Week_7.model;
  */
 
 public class Card {
-    
+    // create variables
+    private final String face;
+    private final String suit;
+    private final int value;
+
+    // constructor
+    public Card(String face, String suit, int value) {
+        this.face = face;
+        this.suit = suit;
+        this.value = value;
+    }
+
+    // getter method for value
+    public int getValue() {
+        return value;
+    }
+
+    // toString method for use later
+    public String toString() {
+        return face + " of " + suit;
+    }
 }
--- model/Deck.java
+++ model/Deck.java
@@ -1,4 +1,5 @@
 package Week_7.model;
+import java.util.*;
 
 /* 
  * Name: Sekai Cuperus
@@ -10,5 +11,45 @@ package Week_7.model;
  */
 
 public class Deck {
+
+    // create vars
+    private final List<Card> deck;
+    private int currentCard;
+    private static final int NUM_CARDS = 52;
+    private static final Random random = new Random();
     
+    // constructor
+    public Deck() {
+        // create deck arrays for card numbers and suits
+        String[] cardNumbers = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+        String[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        // initialize deck
+        deck = new ArrayList<>();
+
+        for (String suit : suits) {
+            for (int i = 0; i < cardNumbers.length; i++) {
+                int value = i + 1;
+                if (value > 10) {
+                    value = 10; // face cards are worth 10
+                }
+                deck.add(new Card(cardNumbers[i], suit, value));
+            }
+        }
+        currentCard = 0;
+    }
+
+    // shuffle method
+    public void shuffle() {
+        Collections.shuffle(deck, random);
+        currentCard = 0;
+    }
+
+    // deal card method
+    public Card dealCard() {
+        if (currentCard < deck.size()) {
+            return deck.get(currentCard++);
+        } else {
+            return null; // no more cards to deal
+        }
+    }
 }
