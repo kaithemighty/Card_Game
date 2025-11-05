0dbf5b197ff717c4d257e051569c4c0a2fe7f733
Sekai Cuperus <ck.cuperus@icloud.com>
2025-11-04 19:55:19 -0800

    Initial commit

---
+++ manager/Blackjack.java
@@ -0,0 +1,14 @@
+package Week_7.manager;
+
+/* 
+ * Name: Sekai Cuperus
+ * Class: Computer Science 2
+ * Instructor: Darrell Criss
+ * Assignment: Week 7 - Card Game
+ * Purpose: Create a Card Game using java, demonstrate use of git
+ *          and OOP concepts
+ */
+
+public class Blackjack {
+    
+}
---
+++ test/GameTest.java
@@ -0,0 +1,14 @@
+package Week_7.test;
+
+/* 
+ * Name: Sekai Cuperus
+ * Class: Computer Science 2
+ * Instructor: Darrell Criss
+ * Assignment: Week 7 - Card Game
+ * Purpose: Create a Card Game using java, demonstrate use of git
+ *          and OOP concepts
+ */
+
+public class GameTest {
+    
+}
